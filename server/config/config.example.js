a7acaba09ab98c55425b4e1597f80f3cfcc9102c
JinitSan <jinitsanghvi@gmail.com>
2022-02-04 11:37:50 +0530

    added mongo string

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,8 +1,9 @@
 //This file holds any configuration variables we may need
 //'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+require('dotenv').config();
 
 module.exports = {
     db: {
-        uri: '', //place the URI of your mongo database here.
+        uri: process.env.MONGO_STR, //place the URI of your mongo database here.
     }
 };
